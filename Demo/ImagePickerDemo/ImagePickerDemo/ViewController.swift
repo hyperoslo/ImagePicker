5994d18b0e7fa1fdb4ca2d17571168e28fc8ebe2
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-08-04 12:06:32 +0200

    Added the project files

---
+++ Demo/ImagePickerDemo/ImagePickerDemo/ViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+}
+
