62bbf2f217ac767807bec91ba44f0e1e24e99326
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-08-04 12:20:41 +0200

    Added new lines

--- Source/ImagePickerController.swift
+++ Source/ImagePickerController.swift
@@ -2,7 +2,15 @@ import UIKit
 
 class ImagePickerController: UIViewController {
 
+  // MARK: - View lifecycle
+
   override func viewDidLoad() {
       super.viewDidLoad()
+  }
+
+  // MARK: - Autolayout
+
+  func setupConstraints() {
+    
   }
 }
