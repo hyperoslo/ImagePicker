af6c5fa50ba231ee626bb078c5ce0635ee1cb448
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-08-04 10:17:57 +0200

    Deleted boilerplate code

--- ImagePickerDemo/ImagePickerDemo/ViewController.swift
+++ ImagePickerDemo/ImagePickerDemo/ViewController.swift
@@ -1,25 +1,13 @@
-//
-//  ViewController.swift
-//  ImagePickerDemo
-//
-//  Created by Ramon Gilabert Llop on 8/4/15.
-//  Copyright (c) 2015 Ramon Gilabert Llop. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
-
-
 }
 
