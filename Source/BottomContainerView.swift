df35ce1daa0c3cbabc0a0b373dec399cdad6e70d
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-08-04 14:43:43 +0200

    Configured different views

---
+++ Source/BottomContainerView.swift
@@ -0,0 +1,121 @@
+import UIKit
+
+protocol BottomContainerViewDelegate {
+
+  func pickerButtonDidPress()
+  func doneButtonDidPress()
+  func cancelButtonDidPress()
+}
+
+class BottomContainerView: UIView {
+
+  lazy var pickerButton: ButtonPicker = {
+    let pickerButton = ButtonPicker()
+    pickerButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+    pickerButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+
+    return pickerButton
+    }()
+
+  lazy var borderPickerButton: UIView = {
+    let view = UIView()
+    view.backgroundColor = UIColor.clearColor()
+    view.layer.borderColor = UIColor.whiteColor().CGColor
+    view.layer.borderWidth = ButtonPicker.Dimensions.borderWidth
+    view.layer.cornerRadius = ButtonPicker.Dimensions.buttonBorderSize / 2
+    view.setTranslatesAutoresizingMaskIntoConstraints(false)
+
+    return view
+    }()
+
+  lazy var doneButton: UIButton = { [unowned self] in
+    let button = UIButton()
+    button.setTitle(self.configuration.cancelButtonTitle, forState: .Normal)
+    button.addTarget(self, action: "doneButtonDidPress:", forControlEvents: .TouchUpInside)
+    button.setTranslatesAutoresizingMaskIntoConstraints(false)
+
+    return button
+    }()
+
+  lazy var configuration: PickerConfiguration = {
+    let configuration = PickerConfiguration()
+    return configuration
+    }()
+
+  var delegate: BottomContainerViewDelegate?
+
+  // MARK: Initializers
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+
+    [borderPickerButton, pickerButton, doneButton].map { self.addSubview($0) }
+
+    setupConstraints()
+  }
+
+  required init(coder aDecoder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: - Autolayout
+
+  func setupConstraints() {
+    let attributesBorder: [NSLayoutAttribute] = [.CenterX, .CenterY]
+
+    attributesBorder.map {
+      self.addConstraint(NSLayoutConstraint(item: self.pickerButton, attribute: $0,
+        relatedBy: .Equal, toItem: self, attribute: $0,
+        multiplier: 1, constant: 0))
+    }
+
+    addConstraint(NSLayoutConstraint(item: pickerButton, attribute: .Width,
+      relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute,
+      multiplier: 1, constant: ButtonPicker.Dimensions.buttonSize))
+
+    addConstraint(NSLayoutConstraint(item: pickerButton, attribute: .Height,
+      relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute,
+      multiplier: 1, constant: ButtonPicker.Dimensions.buttonSize))
+
+    attributesBorder.map {
+      self.addConstraint(NSLayoutConstraint(item: self.borderPickerButton, attribute: $0,
+        relatedBy: .Equal, toItem: self, attribute: $0,
+        multiplier: 1, constant: 0))
+    }
+
+    addConstraint(NSLayoutConstraint(item: borderPickerButton, attribute: .Width,
+      relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute,
+      multiplier: 1, constant: ButtonPicker.Dimensions.buttonBorderSize))
+
+    addConstraint(NSLayoutConstraint(item: borderPickerButton, attribute: .Height,
+      relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute,
+      multiplier: 1, constant: ButtonPicker.Dimensions.buttonBorderSize))
+
+    addConstraint(NSLayoutConstraint(item: doneButton, attribute: .CenterY,
+      relatedBy: .Equal, toItem: self, attribute: .CenterY,
+      multiplier: 1, constant: 0))
+
+    addConstraint(NSLayoutConstraint(item: doneButton, attribute: .CenterX,
+      relatedBy: .Equal, toItem: self, attribute: .Right,
+      multiplier: 1, constant: -(UIScreen.mainScreen().bounds.width - (ButtonPicker.Dimensions.buttonBorderSize + UIScreen.mainScreen().bounds.width)/2)/2))
+  }
+
+  // MARK: - Action methods
+
+  func doneButtonDidPress(button: UIButton) {
+    if button.currentTitle == self.configuration.cancelButtonTitle {
+      delegate?.cancelButtonDidPress()
+    } else {
+      delegate?.doneButtonDidPress()
+    }
+  }
+}
+
+// MARK: - ButtonPickerDelegate methods
+
+extension BottomContainerView: ButtonPickerDelegate {
+
+  func buttonDidPress() {
+    delegate?.pickerButtonDidPress()
+  }
+}
--- Source/ImagePickerController.swift
+++ Source/ImagePickerController.swift
@@ -11,33 +11,13 @@ public class ImagePickerController: UIViewController {
     return galleryView
     }()
 
-  lazy var borderPickerButton: UIView = {
-    let view = UIView()
-    view.backgroundColor = UIColor.clearColor()
-    view.layer.borderColor = UIColor.whiteColor().CGColor
-    view.layer.borderWidth = ButtonPicker.Dimensions.borderWidth
-    view.layer.cornerRadius = ButtonPicker.Dimensions.buttonBorderSize / 2
+  lazy var bottomContainer: BottomContainerView = {
+    let view = BottomContainerView()
+    view.backgroundColor = UIColor.blackColor()
     view.setTranslatesAutoresizingMaskIntoConstraints(false)
+    view.delegate = self
 
     return view
-    }()
-
-  lazy var pickerButton: ButtonPicker = {
-    let pickerButton = ButtonPicker()
-    pickerButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
-    pickerButton.setTranslatesAutoresizingMaskIntoConstraints(false)
-    pickerButton.delegate = self
-
-    return pickerButton
-    }()
-
-  lazy var doneButton: UIButton = { [unowned self] in
-    let button = UIButton()
-    button.setTitle(self.configuration.cancelButtonTitle, forState: .Normal)
-    button.addTarget(self, action: "doneButtonDidPress:", forControlEvents: .TouchUpInside)
-    button.setTranslatesAutoresizingMaskIntoConstraints(false)
-
-    return button
     }()
 
   lazy var configuration: PickerConfiguration = {
@@ -45,17 +25,9 @@ public class ImagePickerController: UIViewController {
     return configuration
     }()
 
-  lazy var bottomContainer: UIView = {
-    let view = UIView()
-    view.backgroundColor = UIColor.blackColor()
-    view.setTranslatesAutoresizingMaskIntoConstraints(false)
-
-    return view
-    }()
-
   public var doneButtonTitle: String? {
     didSet {
-      doneButton.setTitle(doneButtonTitle!, forState: .Normal)
+      bottomContainer.doneButton.setTitle(doneButtonTitle!, forState: .Normal)
     }
   }
 
@@ -65,7 +37,6 @@ public class ImagePickerController: UIViewController {
     super.viewDidLoad()
 
     [bottomContainer].map { self.view.addSubview($0) }
-    [borderPickerButton, pickerButton, doneButton].map { self.bottomContainer.addSubview($0) }
 
     setupConstraints()
   }
@@ -80,7 +51,6 @@ public class ImagePickerController: UIViewController {
 
   func setupConstraints() {
     let attributes: [NSLayoutAttribute] = [.Bottom, .Right, .Width]
-    let attributesBorder: [NSLayoutAttribute] = [.CenterX, .CenterY]
 
     attributes.map {
       self.view.addConstraint(NSLayoutConstraint(item: self.bottomContainer, attribute: $0,
@@ -91,63 +61,18 @@ public class ImagePickerController: UIViewController {
     view.addConstraint(NSLayoutConstraint(item: self.bottomContainer, attribute: .Height,
       relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute,
       multiplier: 1, constant: Dimensions.bottomContainerHeight))
-
-    attributesBorder.map {
-      self.view.addConstraint(NSLayoutConstraint(item: self.pickerButton, attribute: $0,
-        relatedBy: .Equal, toItem: self.bottomContainer, attribute: $0,
-        multiplier: 1, constant: 0))
-    }
-
-    view.addConstraint(NSLayoutConstraint(item: pickerButton, attribute: .Width,
-      relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute,
-      multiplier: 1, constant: ButtonPicker.Dimensions.buttonSize))
-
-    view.addConstraint(NSLayoutConstraint(item: pickerButton, attribute: .Height,
-      relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute,
-      multiplier: 1, constant: ButtonPicker.Dimensions.buttonSize))
-
-    attributesBorder.map {
-      self.view.addConstraint(NSLayoutConstraint(item: self.borderPickerButton, attribute: $0,
-        relatedBy: .Equal, toItem: self.bottomContainer, attribute: $0,
-        multiplier: 1, constant: 0))
-    }
-
-    view.addConstraint(NSLayoutConstraint(item: borderPickerButton, attribute: .Width,
-      relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute,
-      multiplier: 1, constant: ButtonPicker.Dimensions.buttonBorderSize))
-
-    view.addConstraint(NSLayoutConstraint(item: borderPickerButton, attribute: .Height,
-      relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute,
-      multiplier: 1, constant: ButtonPicker.Dimensions.buttonBorderSize))
-
-    view.addConstraint(NSLayoutConstraint(item: doneButton, attribute: .CenterY,
-      relatedBy: .Equal, toItem: bottomContainer, attribute: .CenterY,
-      multiplier: 1, constant: 0))
-
-    view.addConstraint(NSLayoutConstraint(item: doneButton, attribute: .CenterX,
-      relatedBy: .Equal, toItem: bottomContainer, attribute: .Right,
-      multiplier: 1, constant: -(UIScreen.mainScreen().bounds.width - (ButtonPicker.Dimensions.buttonBorderSize + UIScreen.mainScreen().bounds.width)/2)/2))
   }
 }
 
 // MARK: - Action methods
 
-extension ImagePickerController {
+extension ImagePickerController: BottomContainerViewDelegate {
 
-  func doneButtonDidPress(button: UIButton) {
-    if button.currentTitle == self.configuration.cancelButtonTitle {
-      dismissViewControllerAnimated(true, completion: nil)
-    } else {
-      // TODO: Show lightbox with all the images
-    }
-  }
-}
+  func pickerButtonDidPress() { }
 
-// MARK: - ButtonPickerDelegate methods
+  func doneButtonDidPress() { }
 
-extension ImagePickerController: ButtonPickerDelegate {
-
-  func buttonDidPress() {
-    // TODO: Handle button actions
+  func cancelButtonDidPress() {
+    dismissViewControllerAnimated(true, completion: nil)
   }
 }
