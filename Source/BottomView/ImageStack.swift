3265bbefd304b81b45000efdc8064f0e067603f4
richardoti <richard@hyper.no>
2015-08-10 19:45:00 +0200

    Add interface

---
+++ Source/BottomView/ImageStack.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class ImageStack: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+    }
+
+    func pushImage(image: UIImage) {
+
+    }
+
+    func dropImage(image: UIImage) {
+
+    }
+
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
