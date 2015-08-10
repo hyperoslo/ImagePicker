d5e43e39e41c0313f4d3960965cdb6b147acc1dd
richardoti <richard@hyper.no>
2015-08-10 20:01:48 +0200

    Add image storage array

--- Source/BottomView/ImageStack.swift
+++ Source/BottomView/ImageStack.swift
@@ -2,17 +2,23 @@ import UIKit
 
 class ImageStack: UIView {
 
+  var images: NSMutableArray = NSMutableArray()
+
   override init(frame: CGRect) {
     super.init(frame: frame)
 
   }
 
   func pushImage(image: UIImage) {
+    images.insertObject(image, atIndex: 0)
     println("Image pushed")
+    println(images)
   }
 
   func dropImage(image: UIImage) {
+    images.removeObject(image)
     println("Image dropped")
+    println(images)
   }
 
   required init(coder aDecoder: NSCoder) {
