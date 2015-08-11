094bdf91dddf0fff069a971a5fc8083678c533f3
richardoti <richard@hyper.no>
2015-08-11 03:56:41 +0200

    Refactor ImageStack

--- Source/BottomView/ImageStack.swift
+++ Source/BottomView/ImageStack.swift
@@ -8,40 +8,24 @@ struct ImageStack {
 
   static var sharedStack = ImageStack()
 
-  var images: NSMutableArray = NSMutableArray()
+  var images: [UIImage] = [UIImage]()
   var delegate: ImageStackDelegate?
 
-  func pushImage(image: UIImage) {
-    images.insertObject(image, atIndex: 0)
+  mutating func pushImage(image: UIImage) {
+    images.append(image)
     delegate?.imageStackDidReload()
     println("Image pushed")
     println(images)
   }
 
-  func dropImage(image: UIImage) {
-    images.removeObject(image)
+  mutating func dropImage(image: UIImage) {
+    images = images.filter() {$0 != image}
     delegate?.imageStackDidReload()
     println("Image dropped")
     println(images)
   }
 
   func containsImage(image: UIImage) -> Bool {
-    return images.containsObject(image)
-  }
-
-  func getImages() -> [UIImage] {
-//    let size = images.count > 4 ? 3 : images.count
-//    var array = [UIImage]()
-//    for i in 0...size {
-//      array.append(images[i] as! UIImage)
-//    }
-//    return array
-          println("b")
-    var array: [UIImage] = [UIImage]()
-    for image in images {
-      println("a")
-      array.append(image as! UIImage)
-    }
-return array
+    return contains(images, image)
   }
 }
