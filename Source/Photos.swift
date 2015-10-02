f597338d8bf0058e2015b713d769a895caff237a
Christoffer Winterkvist <christoffer@hyper.no>
2015-10-02 15:04:37 +0200

    Add static fetch method with completion

--- Source/Photos.swift
+++ Source/Photos.swift
@@ -3,4 +3,28 @@ import AssetsLibrary
 
 struct Photos {
 
+  static func fetch(completion: (assets: [PHAsset]) -> Void) {
+    let fetchOptions = PHFetchOptions()
+    let authorizationStatus = ALAssetsLibrary.authorizationStatus()
+    var fetchResult: PHFetchResult?
+
+    guard authorizationStatus == .Authorized else { return }
+
+    if fetchResult == nil {
+      fetchResult = PHAsset.fetchAssetsWithMediaType(PHAssetMediaType.Image, options: fetchOptions)
+    }
+
+    if fetchResult?.count > 0 {
+      var assets = [PHAsset]()
+      fetchResult?.enumerateObjectsUsingBlock { object, index, stop in
+        if let asset = object as? PHAsset {
+          assets.append(asset)
+        }
+      }
+
+      dispatch_async(dispatch_get_main_queue(), {
+        completion(assets: assets)
+      })
+    }
+  }
 }
