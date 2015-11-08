8dc2136e49cc46fa163a39db80f573b646d9e8a6
Khoa Pham <onmyway133@gmail.com>
2015-11-08 15:30:52 +0700

    Put the sender first in delegate methods

---
+++ Source/ImagePickerDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  ImagePickerDelegate.swift
+//  Pods
+//
+//  Created by Khoa Pham on 11/8/15.
+//
+//
+
+import Foundation
+
+public protocol ImagePickerDelegate: class {
+    func imagePicker(imagePickerController: ImagePickerController, wrapperDidPress images: [UIImage])
+    func imagePicker(imagePickerController: ImagePickerController, doneButtonDidPress images: [UIImage])
+    func imagePickerCancelButtonDidPress(imagePickerController: ImagePickerController)
+}
