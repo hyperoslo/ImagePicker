79b33fa313b96e85684fc97538b8dced4ebe0e68
Khoa Pham <onmyway133@gmail.com>
2015-11-08 15:32:17 +0700

    Make wrapperDidPress and imagePickerCancelButtonDidPress optional using public protocol extension with default implementation

--- Source/ImagePickerDelegate.swift
+++ Source/ImagePickerDelegate.swift
@@ -13,3 +13,8 @@ public protocol ImagePickerDelegate: class {
     func imagePicker(imagePickerController: ImagePickerController, doneButtonDidPress images: [UIImage])
     func imagePickerCancelButtonDidPress(imagePickerController: ImagePickerController)
 }
+
+public extension ImagePickerDelegate {
+    func imagePicker(imagePickerController: ImagePickerController, wrapperDidPress images: [UIImage]) {}
+    func imagePickerCancelButtonDidPress(imagePickerController: ImagePickerController) {}
+}
