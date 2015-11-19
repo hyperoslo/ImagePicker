d797e759fe00d4265c39f02d43261ef57ba003d3
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-11-19 09:18:07 +0100

    Adds static everywhere

--- Source/Configuration.swift
+++ Source/Configuration.swift
@@ -8,28 +8,28 @@ public struct Configuration {
 
   // MARK: Colors
 
-  public var backgroundColor = UIColor(red:0.15, green:0.19, blue:0.24, alpha:1)
-  public var mainColor = UIColor(red:0.09, green:0.11, blue:0.13, alpha:1)
-  public var noImagesColor = UIColor(red:0.86, green:0.86, blue:0.86, alpha:1)
-  public var noCameraColor = UIColor(red:0.86, green:0.86, blue:0.86, alpha:1)
-  public var settingsColor = UIColor.whiteColor()
+  public static var backgroundColor = UIColor(red:0.15, green:0.19, blue:0.24, alpha:1)
+  public static var mainColor = UIColor(red:0.09, green:0.11, blue:0.13, alpha:1)
+  public static var noImagesColor = UIColor(red:0.86, green:0.86, blue:0.86, alpha:1)
+  public static var noCameraColor = UIColor(red:0.86, green:0.86, blue:0.86, alpha:1)
+  public static var settingsColor = UIColor.whiteColor()
 
   // MARK: Fonts
 
-  public var numberLabelFont = UIFont(name: "HelveticaNeue-Bold", size: 19)!
-  public var doneButton = UIFont(name: "HelveticaNeue-Medium", size: 19)!
-  public var flashButton = UIFont(name: "HelveticaNeue-Medium", size: 12)!
-  public var noImagesFont = UIFont(name: "HelveticaNeue-Medium", size: 18)!
-  public var noCameraFont = UIFont(name: "HelveticaNeue-Medium", size: 18)!
-  public var settingsFont = UIFont(name: "HelveticaNeue-Medium", size: 18)!
+  public static var numberLabelFont = UIFont(name: "HelveticaNeue-Bold", size: 19)!
+  public static var doneButton = UIFont(name: "HelveticaNeue-Medium", size: 19)!
+  public static var flashButton = UIFont(name: "HelveticaNeue-Medium", size: 12)!
+  public static var noImagesFont = UIFont(name: "HelveticaNeue-Medium", size: 18)!
+  public static var noCameraFont = UIFont(name: "HelveticaNeue-Medium", size: 18)!
+  public static var settingsFont = UIFont(name: "HelveticaNeue-Medium", size: 18)!
 
   // MARK: Titles
 
-  public var cancelButtonTitle = "Cancel"
-  public var doneButtonTitle = "Done"
-  public var noImagesTitle = "No images available"
-  public var noCameraTitle = "Camera is not available"
-  public var settingsTitle = "Settings"
+  public static var cancelButtonTitle = "Cancel"
+  public static var doneButtonTitle = "Done"
+  public static var noImagesTitle = "No images available"
+  public static var noCameraTitle = "Camera is not available"
+  public static var settingsTitle = "Settings"
 
   // MARK: Dimensions
 
