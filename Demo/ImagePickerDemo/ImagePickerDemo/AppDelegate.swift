10f6405195430ace635870bc18c509cfb2edae7e
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-12-11 10:50:44 +0100

    Refactored the demo

--- Demo/ImagePickerDemo/ImagePickerDemo/AppDelegate.swift
+++ Demo/ImagePickerDemo/ImagePickerDemo/AppDelegate.swift
@@ -4,15 +4,8 @@ import ImagePicker
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-  lazy var imagePickerController: ImagePickerController = {
-    let imagePickerController = ImagePickerController()
-    return imagePickerController
-    }()
-
-  lazy var window: UIWindow? = {
-    let window = UIWindow(frame: UIScreen.mainScreen().bounds)
-    return window
-    }()
+  lazy var imagePickerController: ImagePickerController = ImagePickerController()
+  lazy var window: UIWindow? = UIWindow(frame: UIScreen.mainScreen().bounds)
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     window?.rootViewController = imagePickerController
