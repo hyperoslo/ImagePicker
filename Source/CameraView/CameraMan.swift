e5b1468927dad4534a62a6b9541ba2f0a4a8536c
Khoa Pham <onmyway133@gmail.com>
2016-04-28 14:56:23 +0200

    Input and output

--- Source/CameraView/CameraMan.swift
+++ Source/CameraView/CameraMan.swift
@@ -6,18 +6,25 @@ class CameraMan {
 
   let session = AVCaptureSession()
   let queue = dispatch_queue_create("no.hyper.ImagePicker.Camera.SessionQueue", dispatch_queue_attr_make_with_qos_class(DISPATCH_QUEUE_SERIAL, QOS_CLASS_BACKGROUND, 0))
-  var backCamera: AVCaptureDevice?
-  var frontCamera: AVCaptureDevice?
+  
+  var backCamera: AVCaptureDeviceInput?
+  var frontCamera: AVCaptureDeviceInput?
+  var stillImageOutput: AVCaptureStillImageOutput?
 
   var noCameraHandler: (() -> Void)?
+
+  deinit {
+    stop()
+  }
 
   // MARK: - Setup
 
   func setup() {
-    
+    checkPermission()
   }
 
   func setupDevices() {
+    // Input
     AVCaptureDevice
     .devices().flatMap {
       return $0 as? AVCaptureDevice
@@ -26,14 +33,17 @@ class CameraMan {
     }.forEach {
       switch $0.position {
       case .Front:
-        self.frontCamera = $0
+        self.frontCamera = try? AVCaptureDeviceInput(device: $0)
       case .Back:
-        self.backCamera = $0
+        self.backCamera = try? AVCaptureDeviceInput(device: $0)
       default:
         break
       }
     }
 
+    // Output
+    stillImageOutput = AVCaptureStillImageOutput()
+    stillImageOutput?.outputSettings = [AVVideoCodecKey: AVVideoCodecJPEG]
   }
 
   // MARK: - Permission
@@ -41,7 +51,7 @@ class CameraMan {
   func checkPermission() {
     let status = AVCaptureDevice.authorizationStatusForMediaType(AVMediaTypeVideo)
 
-    switch  status {
+    switch status {
     case .Authorized:
       setupDevices()
     case .NotDetermined:
@@ -61,5 +71,66 @@ class CameraMan {
         }
       }
     }
+  }
+
+  // MARK: - Session
+
+  func start() {
+    guard let input = backCamera, output = stillImageOutput else { return }
+
+    configurePreset(input)
+
+    if session.canAddInput(input) {
+      session.addInput(input)
+    }
+
+    if session.canAddOutput(output) {
+      session.addOutput(output)
+    }
+
+    dispatch_async(queue) {
+      self.session.startRunning()
+    }
+  }
+
+  func stop() {
+    dispatch_async(queue) {
+      self.session.stopRunning()
+    }
+  }
+
+  func switchCamera() {
+    guard let currentInput = session.inputs.first as? AVCaptureDeviceInput else { return }
+
+    dispatch_async(queue) {
+      self.session.beginConfiguration()
+
+      guard let input = (currentInput == self.backCamera) ? self.frontCamera : self.backCamera else { return }
+
+      self.session.removeInput(currentInput)
+      self.configurePreset(input)
+      self.session.addInput(input)
+
+      self.session.commitConfiguration()
+    }
+  }
+
+  // MARK: - Preset
+
+  func configurePreset(input: AVCaptureDeviceInput) {
+    preferredPresets().forEach {
+      if input.device.supportsAVCaptureSessionPreset($0) && self.session.canSetSessionPreset($0) {
+        self.session.sessionPreset = $0
+        return
+      }
+    }
+  }
+
+  func preferredPresets() -> [String] {
+    return [
+      AVCaptureSessionPresetHigh,
+      AVCaptureSessionPresetMedium,
+      AVCaptureSessionPresetLow
+    ]
   }
 }
