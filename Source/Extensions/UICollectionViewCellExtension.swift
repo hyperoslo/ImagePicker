6b801fd30aefa31da4a8798caf63fa47664b0ce9
Diogo Antunes <diogo.antunes@gmail.com>
2016-06-14 12:38:50 +0100

    Added:- IntExtension for syntactic sugar in repeating a closure- UICollectionViewExtension to make use of generics in order to register and dequeue a cell- UICollectionViewCellExtension to conform to a protocol assuring a reusable identifier for the cellsRefactored:- struct Dimensions present in most classes changed to an enum to assure better readability and less error prone, since empty enums cannot be created;- removed “CameraView” conformace to the “CLLocationManagerDelegate” since this moved to it's own class "LocationManager";- “CameraView” extension to conform to the "CameraManDelegate" for better readability and separation of responsibilities;- renamed lazy vars to a new syntax sugar that assures better readability;

---
+++ Source/Extensions/UICollectionViewCellExtension.swift
@@ -0,0 +1,20 @@
+//
+//  UICollectionViewCell+Extension.swift
+//  ImagePicker
+//
+//  Created by Diogo Antunes on 6/14/16.
+//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
+//
+
+import UIKit
+
+protocol ReusableView { }
+
+extension ReusableView where Self: UIView {
+  
+  static var reuseIdentifier: String {
+    return String(self)
+  }
+}
+
+extension UICollectionViewCell: ReusableView { }
---
+++ Source/Extensions/UICollectionViewExtension.swift
@@ -0,0 +1,9 @@
+//
+//  UICollectionViewExtension.swift
+//  ImagePicker
+//
+//  Created by Diogo Antunes on 6/14/16.
+//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
+//
+
+import Foundation
