011ac465d8d35a4db1dd3a193f87a9cf9899cb45
Khoa Pham <onmyway133@gmail.com>
2016-08-04 10:15:49 +0200

    Present ImagePicker

--- Demo/ImagePickerDemo/ImagePickerDemo/AppDelegate.swift
+++ Demo/ImagePickerDemo/ImagePickerDemo/AppDelegate.swift
@@ -1,39 +1,15 @@
 import UIKit
-import ImagePicker
-import Lightbox
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, ImagePickerDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
-  lazy var imagePickerController: ImagePickerController = ImagePickerController()
   lazy var window: UIWindow? = UIWindow(frame: UIScreen.mainScreen().bounds)
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
-    imagePickerController.delegate = self
-
-    window?.rootViewController = imagePickerController
+    window?.rootViewController = ViewController()
     window?.makeKeyAndVisible()
     
     return true
-  }
-
-  // MARK: - ImagePickerDelegate
-
-  func cancelButtonDidPress(imagePicker: ImagePickerController) {
-
-  }
-
-  func wrapperDidPress(imagePicker: ImagePickerController, images: [UIImage]) {
-    let lightboxImages = images.map {
-      return LightboxImage(image: $0)
-    }
-
-    let lightbox = LightboxController(images: lightboxImages, startIndex: 0)
-    window?.rootViewController?.presentViewController(lightbox, animated: true, completion: nil)
-  }
-
-  func doneButtonDidPress(imagePicker: ImagePickerController, images: [UIImage]) {
-
   }
 }
---
+++ Demo/ImagePickerDemo/ImagePickerDemo/ViewController.swift
@@ -0,0 +1,54 @@
+import UIKit
+import ImagePicker
+import Lightbox
+
+class ViewController: UIViewController, ImagePickerDelegate {
+
+  lazy var button: UIButton = self.makeButton()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    view.backgroundColor = UIColor.whiteColor()
+    view.addSubview(button)
+    button.translatesAutoresizingMaskIntoConstraints = false
+
+    view.addConstraint(NSLayoutConstraint(item: button, attribute: .CenterX, relatedBy: .Equal, toItem: view, attribute: .CenterX, multiplier: 1, constant: 0))
+    view.addConstraint(NSLayoutConstraint(item: button, attribute: .CenterY, relatedBy: .Equal, toItem: view, attribute: .CenterY, multiplier: 1, constant: 0))
+  }
+
+  func makeButton() -> UIButton {
+    let button = UIButton()
+    button.setTitle("Show ImagePicker", forState: .Normal)
+    button.setTitleColor(UIColor.blackColor(), forState: .Normal)
+    button.addTarget(self, action: #selector(buttonTouched(_:)), forControlEvents: .TouchUpInside)
+
+    return button
+  }
+
+  func buttonTouched(button: UIButton) {
+    let imagePicker = ImagePickerController()
+    imagePicker.delegate = self
+
+    presentViewController(imagePicker, animated: true, completion: nil)
+  }
+
+  // MARK: - ImagePickerDelegate
+
+  func cancelButtonDidPress(imagePicker: ImagePickerController) {
+    imagePicker.dismissViewControllerAnimated(true, completion: nil)
+  }
+
+  func wrapperDidPress(imagePicker: ImagePickerController, images: [UIImage]) {
+    let lightboxImages = images.map {
+      return LightboxImage(image: $0)
+    }
+
+    let lightbox = LightboxController(images: lightboxImages, startIndex: 0)
+    presentViewController(lightbox, animated: true, completion: nil)
+  }
+
+  func doneButtonDidPress(imagePicker: ImagePickerController, images: [UIImage]) {
+    imagePicker.dismissViewControllerAnimated(true, completion: nil)
+  }
+}
