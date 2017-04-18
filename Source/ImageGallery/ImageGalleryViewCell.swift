5a068fb51fdd1deb9e5f99b5f6ba75ea3539c14c
Michal Pyrka <michal.pyrka@droidsonroids.pl>
2017-04-18 17:40:15 +0200

    Removed whitespaces.

--- Source/ImageGallery/ImageGalleryViewCell.swift
+++ Source/ImageGallery/ImageGalleryViewCell.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class ImageGalleryViewCell: UICollectionViewCell {
-  
+
   lazy var imageView = UIImageView()
   lazy var selectedImageView = UIImageView()
   private var videoInfoView: VideoInfoView
@@ -23,26 +23,26 @@ class ImageGalleryViewCell: UICollectionViewCell {
                                width: frame.width, height: self.videoInfoBarHeight)
     videoInfoView = VideoInfoView(frame: videoBarFrame)
     super.init(frame: frame)
-    
+
     for view in [imageView, selectedImageView, videoInfoView] as [UIView] {
       view.contentMode = .scaleAspectFill
       view.translatesAutoresizingMaskIntoConstraints = false
       view.clipsToBounds = true
       contentView.addSubview(view)
     }
-    
+
     isAccessibilityElement = true
     accessibilityLabel = "Photo"
-    
+
     setupConstraints()
   }
-  
+
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-  
+
   // MARK: - Configuration
-  
+
   func configureCell(_ image: UIImage) {
     imageView.image = image
   }
--- Source/ImageGallery/VideoInfoView.swift
+++ Source/ImageGallery/VideoInfoView.swift
@@ -1,13 +1,13 @@
 import UIKit
 
 class VideoInfoView: UIView {
-  
+
   var duration: TimeInterval? {
     didSet {
       videoInfoLabel.text = dateFormatter.string(from: duration ?? 0)
     }
   }
-  
+
   private lazy var videoIcon: UIImageView = {
     var videoIcon = UIImageView(image: AssetManager.getImage("video"))
     videoIcon.frame = CGRect(x: 3,
@@ -17,7 +17,7 @@ class VideoInfoView: UIView {
     videoIcon.contentMode = .scaleAspectFit
     return videoIcon
   }()
-  
+    
   private lazy var videoInfoLabel: UILabel = {
     let videoInfoLabel = UILabel(frame: CGRect(x: 0,
                                                y: 0,
@@ -29,7 +29,7 @@ class VideoInfoView: UIView {
     videoInfoLabel.text = self.dateFormatter.string(from: self.duration ?? 0)
     return videoInfoLabel
   }()
-  
+    
   private lazy var dateFormatter: DateComponentsFormatter = {
     let formatter = DateComponentsFormatter()
     formatter.zeroFormattingBehavior = .pad
@@ -37,15 +37,15 @@ class VideoInfoView: UIView {
     formatter.unitsStyle = .positional
     return formatter
   }()
-  
+
   override init(frame: CGRect) {
     super.init(frame: frame)
-    
+
     backgroundColor = UIColor(white: 0, alpha: 0.5)
     addSubview(self.videoIcon)
     addSubview(self.videoInfoLabel)
   }
-  
+
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
