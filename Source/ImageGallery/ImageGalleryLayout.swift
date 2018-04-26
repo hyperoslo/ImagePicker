f68483b82e88555d88c6ac7cd4f7db24e82a6608
dulgan <antoine.harlin@gmail.com>
2018-04-26 14:22:56 +0200

    Remove SwiftLint complaining about Identifier Name

--- Source/ImageGallery/ImageGalleryLayout.swift
+++ Source/ImageGallery/ImageGalleryLayout.swift
@@ -21,9 +21,9 @@ class ImageGalleryLayout: UICollectionViewFlowLayout {
     var newAttributes = [UICollectionViewLayoutAttributes]()
     for attribute in attributes {
       // swiftlint:disable force_cast
-      let n = attribute.copy() as! UICollectionViewLayoutAttributes
-      n.transform = configuration.rotationTransform
-      newAttributes.append(n)
+      let attributeCopy = attribute.copy() as! UICollectionViewLayoutAttributes
+      attributeCopy.transform = configuration.rotationTransform
+      newAttributes.append(attributeCopy)
     }
 
     return newAttributes
