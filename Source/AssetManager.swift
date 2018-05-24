f79926ab3e27ee65030162a2ab7b11ece0053e88
David FABREGUETTE <david@appliserv.fr>
2018-05-24 12:09:15 +0200

    temporary fix for : https://github.com/hyperoslo/ImagePicker/issues/315

--- Source/AssetManager.swift
+++ Source/AssetManager.swift
@@ -36,12 +36,13 @@ open class AssetManager {
     }
   }
 
-  open static func resolveAsset(_ asset: PHAsset, size: CGSize = CGSize(width: 720, height: 1280), shouldPreferLowRes: Bool = false, completion: @escaping (_ image: UIImage?) -> Void) {
+  open static func resolveAsset(_ asset: PHAsset, size: CGSize = CGSize(width: 720, height: 1280), completion: @escaping (_ image: UIImage?) -> Void) {
     let imageManager = PHImageManager.default()
     let requestOptions = PHImageRequestOptions()
-    requestOptions.deliveryMode = shouldPreferLowRes ? .fastFormat : .highQualityFormat
+    requestOptions.deliveryMode = .highQualityFormat
     requestOptions.isNetworkAccessAllowed = true
 
+    print("in image picker")
     imageManager.requestImage(for: asset, targetSize: size, contentMode: .aspectFill, options: requestOptions) { image, info in
       if let info = info, info["PHImageFileUTIKey"] == nil {
         DispatchQueue.main.async(execute: {
@@ -55,14 +56,15 @@ open class AssetManager {
     let imageManager = PHImageManager.default()
     let requestOptions = PHImageRequestOptions()
     requestOptions.isSynchronous = true
+    requestOptions.isNetworkAccessAllowed = true
 
     var images = [UIImage]()
     for asset in assets {
-      imageManager.requestImage(for: asset, targetSize: size, contentMode: .aspectFill, options: requestOptions) { image, _ in
-        if let image = image {
-          images.append(image)
+        imageManager.requestImage(for: asset, targetSize: size, contentMode: .aspectFill, options: requestOptions) { image, info in
+            if let image = image {
+                images.append(image)
+            }
         }
-      }
     }
     return images
   }
