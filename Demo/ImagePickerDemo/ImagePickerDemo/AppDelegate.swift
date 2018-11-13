3684fac7b8347b34817ea6c41ce8895f4bc5913b
mariovillamizar <mario.villamizar@hotmail.com>
2018-11-12 20:22:14 -0500

    Migrating to Swift 4.2

--- Demo/ImagePickerDemo/ImagePickerDemo/AppDelegate.swift
+++ Demo/ImagePickerDemo/ImagePickerDemo/AppDelegate.swift
@@ -7,11 +7,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     window = UIWindow()
     window?.rootViewController = controller
     window?.makeKeyAndVisible()
-
+    
     return true
   }
+
 }
