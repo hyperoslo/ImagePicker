fcdf7a7ef22bb2e27fbf5686a52a87afd46bb4c1
abdul ahad <abdul_ahad1996@live.com>
2019-08-15 17:43:06 +0500

    updated to swift 5

--- Source/TopView/TopView.swift
+++ Source/TopView/TopView.swift
@@ -21,10 +21,10 @@ open class TopView: UIView {
 
   open lazy var flashButton: UIButton = { [unowned self] in
     let button = UIButton()
-    button.setImage(AssetManager.getImage("AUTO"), for: UIControlState())
-    button.setTitle("AUTO", for: UIControlState())
+    button.setImage(AssetManager.getImage("AUTO"), for: UIControl.State())
+    button.setTitle("AUTO", for: UIControl.State())
     button.titleEdgeInsets = UIEdgeInsets(top: 0, left: 4, bottom: 0, right: 0)
-    button.setTitleColor(UIColor.white, for: UIControlState())
+    button.setTitleColor(UIColor.white, for: UIControl.State())
     button.setTitleColor(UIColor.white, for: .highlighted)
     button.titleLabel?.font = self.configuration.flashButton
     button.addTarget(self, action: #selector(flashButtonDidPress(_:)), for: .touchUpInside)
@@ -39,7 +39,7 @@ open class TopView: UIView {
     let button = UIButton()
     button.accessibilityLabel = ""
     button.accessibilityHint = "Double-tap to rotate camera"
-    button.setImage(AssetManager.getImage("cameraIcon"), for: UIControlState())
+    button.setImage(AssetManager.getImage("cameraIcon"), for: UIControl.State())
     button.addTarget(self, action: #selector(rotateCameraButtonDidPress(_:)), for: .touchUpInside)
     button.imageView?.contentMode = .center
 
@@ -96,18 +96,18 @@ open class TopView: UIView {
 
     switch currentFlashIndex {
     case 1:
-      button.setTitleColor(UIColor(red: 0.98, green: 0.98, blue: 0.45, alpha: 1), for: UIControlState())
+      button.setTitleColor(UIColor(red: 0.98, green: 0.98, blue: 0.45, alpha: 1), for: UIControl.State())
       button.setTitleColor(UIColor(red: 0.52, green: 0.52, blue: 0.24, alpha: 1), for: .highlighted)
 
     default:
-      button.setTitleColor(UIColor.white, for: UIControlState())
+      button.setTitleColor(UIColor.white, for: UIControl.State())
       button.setTitleColor(UIColor.white, for: .highlighted)
     }
 
     let newTitle = flashButtonTitles[currentFlashIndex]
 
-    button.setImage(AssetManager.getImage(newTitle), for: UIControlState())
-    button.setTitle(newTitle, for: UIControlState())
+    button.setImage(AssetManager.getImage(newTitle), for: UIControl.State())
+    button.setTitle(newTitle, for: UIControl.State())
     button.accessibilityLabel = "Flash mode is \(newTitle)"
 
     delegate?.flashButtonDidPress(newTitle)
