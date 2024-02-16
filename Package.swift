88c0496a7e0a8ab107d20325b19da62734b9a683
jpldev <jpldev@MacBook-Pro-de-jpldev.local>
2024-02-16 07:38:30 +0100

    Upgrade: min version iOS 13+

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "ImagePicker",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
